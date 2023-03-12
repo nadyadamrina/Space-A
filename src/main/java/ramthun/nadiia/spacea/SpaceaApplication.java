a96f9b4ace4fdd06ec0240db5cea100ee4e55216
Nadiia Ramthun <ramthun.n@northeastern.edu>
2023-03-12 12:29:37 -0700

    Initial commit

---
+++ src/main/java/ramthun/nadiia/spacea/SpaceaApplication.java
@@ -0,0 +1,13 @@
+package ramthun.nadiia.spacea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ramthun/nadiia/spacea/SpaceaApplicationTests.java
@@ -0,0 +1,13 @@
+package ramthun.nadiia.spacea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
